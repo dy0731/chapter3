a89894b81b3fb33eda6a299f8ab7c8b3dd1507a2
user <user@user-PC>
2018-12-01 17:40:57 +0900

    hashCode override 적용

--- src/collection/Money.java
+++ src/collection/Money.java
@@ -12,13 +12,13 @@ public class Money {
 		return "Money [value=" + value + "]";
 	}
 
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + value;
-//		return result;
-//	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + value;
+		return result;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
